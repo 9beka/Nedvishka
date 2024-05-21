e3af2a649f551954454f0099acda7685fe1d345e
aleksandr-lihodeev <13thjavascript@gmail.com>
2024-05-21 12:23:27 +0600

    api change

--- src/shared/config/api/api.js
+++ src/shared/config/api/api.js
@@ -1,15 +1,15 @@
-const API = 'http://localhost:5000/'
-// const API = 'https://nedvishka-server.onrender.com/'
+// const API = 'http://localhost:5000/'
+const API = "https://nedvishka-server.onrender.com/";
 
-export const AUTH_API = API + 'auth/'
+export const AUTH_API = API + "auth/";
 
-export const ADS_API = API + 'cards/'
+export const ADS_API = API + "cards/";
 
-export const REGISTER_API = AUTH_API + 'register'
+export const REGISTER_API = AUTH_API + "register";
 
-export const LOGIN_API = AUTH_API + 'login'
+export const LOGIN_API = AUTH_API + "login";
 
-export const ADS_POST_API = ADS_API + 'post'
+export const ADS_POST_API = ADS_API + "post";
 
-export const ADS_GET_OWNERS_API = ADS_API + 'getOwners'
-export const ADS_DELETE_OWNERS_API = ADS_API + 'delete/'
+export const ADS_GET_OWNERS_API = ADS_API + "getOwners";
+export const ADS_DELETE_OWNERS_API = ADS_API + "delete/";
